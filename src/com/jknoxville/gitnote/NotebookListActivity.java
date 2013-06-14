54be6b074e915bc69031f087f302370db6a1ee23
John Knox <knoxboy@gmail.com>
2013-06-14 20:15:48 +0100

    Initial commit. UI work started.

---
+++ src/com/jknoxville/gitnote/NotebookListActivity.java
@@ -0,0 +1,105 @@
+package com.jknoxville.gitnote;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+/**
+ * An activity representing a list of Notebooks. This activity has different
+ * presentations for handset and tablet-size devices. On handsets, the activity
+ * presents a list of items, which when touched, lead to a
+ * {@link NotebookDetailActivity} representing item details. On tablets, the
+ * activity presents the list of items and item details side-by-side using two
+ * vertical panes.
+ * <p>
+ * The activity makes heavy use of fragments. The list of items is a
+ * {@link NotebookListFragment} and the item details (if present) is a
+ * {@link NotebookDetailFragment}.
+ * <p>
+ * This activity also implements the required
+ * {@link NotebookListFragment.Callbacks} interface to listen for item
+ * selections.
+ */
+public class NotebookListActivity extends FragmentActivity implements
+		NotebookListFragment.Callbacks {
+
+	/**
+	 * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+	 * device.
+	 */
+	private boolean mTwoPane;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_notebook_list);
+
+		if (findViewById(R.id.notebook_detail_container) != null) {
+			// The detail container view will be present only in the
+			// large-screen layouts (res/values-large and
+			// res/values-sw600dp). If this view is present, then the
+			// activity should be in two-pane mode.
+			mTwoPane = true;
+
+			// In two-pane mode, list items should be given the
+			// 'activated' state when touched.
+			((NotebookListFragment) getSupportFragmentManager()
+					.findFragmentById(R.id.notebook_list))
+					.setActivateOnItemClick(true);
+		}
+
+		// TODO: If exposing deep links into your app, handle intents here.
+	}
+
+	/**
+	 * Callback method from {@link NotebookListFragment.Callbacks} indicating
+	 * that the item with the given ID was selected.
+	 */
+	@Override
+	public void onItemSelected(String id) {
+		if (mTwoPane) {
+			// In two-pane mode, show the detail view in this activity by
+			// adding or replacing the detail fragment using a
+			// fragment transaction.
+			Bundle arguments = new Bundle();
+			arguments.putString(NotebookDetailFragment.ARG_ITEM_ID, id);
+			NotebookDetailFragment fragment = new NotebookDetailFragment();
+			fragment.setArguments(arguments);
+			getSupportFragmentManager().beginTransaction()
+					.replace(R.id.notebook_detail_container, fragment).commit();
+
+		} else {
+			// In single-pane mode, simply start the detail activity
+			// for the selected item ID.
+			Intent detailIntent = new Intent(this, NotebookDetailActivity.class);
+			detailIntent.putExtra(NotebookDetailFragment.ARG_ITEM_ID, id);
+			startActivity(detailIntent);
+		}
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu_notebook_list, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		//Handle item selection
+		switch (item.getItemId()) {
+		case R.id.new_notebook:
+			newNotebook();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+	
+	private void newNotebook() {
+		//TODO add new notebook using name entry dialog
+	}
+}
